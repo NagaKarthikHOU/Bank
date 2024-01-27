6caf7f52f76d97da8bf9728408451a724ce1e736
NagaKarthikHOU <156093253+NagaKarthikHOU@users.noreply.github.com>
2024-01-27 20:16:58 +0530

    Update BankService.cs

--- BankService.cs
+++ BankService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,10 @@ namespace Bank
 {
     class BankService
     {
-        public string bankId;
         public Bank bank;
         public BankService(string bankId)
         {
-            this.bankId = bankId;
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
-        }
-        public static void GetBank(string bankId)
-        {
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
+            bank = CentralBank.banks.Find(x => x.BankId == bankId);
         }
         
         public void CreateBankStaff(string name, string password)
@@ -28,17 +22,14 @@ namespace Bank
         }
         public Staff AuthenticateStaff(string name, string password)
         {
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
             return bank.staff.Find(u => u.Name == name && u.Password == password);
         }
         public Account AuthenticateUser(string username, string password)
         {
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
             return bank.accounts.Find(u => u.AccountHolderName == username && u.Password == password);
         }
         public decimal ConvertCurrency(string currency, decimal amount)
         {
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
             amount *=  bank.currency[currency];
             return amount;
         }
