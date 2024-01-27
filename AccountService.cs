577a046b86588cb77b9499e5841f478ad097903a
NagaKarthikHOU <156093253+NagaKarthikHOU@users.noreply.github.com>
2024-01-27 20:18:17 +0530

    Update AccountService.cs

--- AccountService.cs
+++ AccountService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace Bank
         public Bank bank;
         public AccountService(string bankId)
         {
-            Bank bank = CentralBank.banks.Find(x => x.BankId == bankId);
+            bank = CentralBank.banks.Find(x => x.BankId == bankId);
         }
         public void Deposite(Account account, decimal amount)
         {
